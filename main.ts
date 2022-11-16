0f4bc19fb95ef8a304ef15e5a03f0df7a560bf14
Elliott-Vallee <116816433+Elliott-Vallee@users.noreply.github.com>
2022-11-16 14:29:54 -0500

    Fichiers initiaux du projet MakeCode

---
+++ main.ts
@@ -0,0 +1,44 @@
+let Piéton = 0
+function Jaune () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+}
+function Rouge () {
+    pins.digitalWritePin(DigitalPin.P2, 1)
+}
+function Orange () {
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+input.onButtonPressed(Button.A, function () {
+    Piéton = 1
+})
+function Vert () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+}
+function Blanc () {
+    pins.digitalWritePin(DigitalPin.P16, 1)
+}
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    Piéton = 0
+    Vert()
+    basic.pause(10000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    Jaune()
+    basic.pause(3000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    Rouge()
+    Orange()
+    basic.pause(10000)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    if (Piéton == 1) {
+        Rouge()
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        Blanc()
+        basic.pause(10000)
+        Piéton = 0
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+})
